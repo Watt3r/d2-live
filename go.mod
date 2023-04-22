cbf2748fa1dae77820fed887e0ea113f9211eadb
Watt3r <git@lucas.tools>
2023-04-22 14:27:33 -0400

    Hello gopher

---
+++ go.mod
@@ -0,0 +1,40 @@
+module github.com/watt3r/d2-live
+
+go 1.20
+
+require (
+	github.com/husobee/vestigo v1.1.1
+	oss.terrastruct.com/d2 v0.4.1
+	oss.terrastruct.com/util-go v0.0.0-20230412185012-06c6224a6f77
+)
+
+require (
+	cdr.dev/slog v1.4.2-0.20221206192828-e4803b10ae17 // indirect
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/alecthomas/chroma v0.10.0 // indirect
+	github.com/alecthomas/chroma/v2 v2.5.0 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/dlclark/regexp2 v1.8.1 // indirect
+	github.com/dop251/goja v0.0.0-20230122112309-96b1610dd4f7 // indirect
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/go-sourcemap/sourcemap v2.1.3+incompatible // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/jung-kurt/gofpdf v1.16.2 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-colorable v0.1.9 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mazznoer/csscolorparser v0.1.3 // indirect
+	github.com/rivo/uniseg v0.4.3 // indirect
+	github.com/yuin/goldmark v1.5.3 // indirect
+	go.opencensus.io v0.24.0 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/exp v0.0.0-20221126150942-6ab00d035af9 // indirect
+	golang.org/x/image v0.3.0 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/term v0.6.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
+	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
+	gonum.org/v1/plot v0.12.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"bytes"
+	"compress/flate"
+	"context"
+	"encoding/base64"
+	"errors"
+	"io"
+	"log"
+        "sort"
+	"net/http"
+
+	"github.com/husobee/vestigo"
+	"oss.terrastruct.com/d2/d2layouts/d2dagrelayout"
+	"oss.terrastruct.com/d2/d2lib"
+	"oss.terrastruct.com/d2/d2renderers/d2svg"
+	"oss.terrastruct.com/d2/lib/textmeasure"
+        	"oss.terrastruct.com/d2/d2graph"
+)
+
+var compressionDict = "->" +
+	"<-" +
+	"--" +
+	"<->"
+
+// Decode decodes a compressed base64 D2 string.
+func Decode(encoded string) (_ string, err error) {
+	b64Decoded, err := base64.URLEncoding.DecodeString(encoded)
+	if err != nil {
+		return "", err
+	}
+
+	zr := flate.NewReaderDict(bytes.NewReader(b64Decoded), []byte(compressionDict))
+	var b bytes.Buffer
+	if _, err := io.Copy(&b, zr); err != nil {
+		return "", err
+	}
+	if err := zr.Close(); err != nil {
+		return "", nil
+	}
+	return b.String(), nil
+}
+
+func init() {
+	var common []string
+	for k := range d2graph.StyleKeywords {
+		common = append(common, k)
+	}
+	for k := range d2graph.ReservedKeywords {
+		common = append(common, k)
+	}
+	for k := range d2graph.ReservedKeywordHolders {
+		common = append(common, k)
+	}
+	sort.Strings(common)
+	for _, k := range common {
+		compressionDict += k
+	}
+}
+
+func GetD2SVGHandler(rw http.ResponseWriter, req *http.Request) {
+	ctx := context.Background()
+
+	svg, err := handleGetD2SVG(ctx, req)
+
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	rw.Header().Set("Content-Type", "image/svg+xml")
+	rw.Write(svg)
+}
+
+func handleGetD2SVG(ctx context.Context, req *http.Request) ([]byte, error) {
+	urlencoded := vestigo.Param(req, "encodedD2")
+	decoded, err := Decode(urlencoded)
+	if err != nil {
+		return nil, errors.New("Invalid Base64 data.")
+	}
+
+	ruler, _ := textmeasure.NewRuler()
+
+	diagram, _, _ := d2lib.Compile(ctx, decoded, &d2lib.CompileOptions{
+		Layout: d2dagrelayout.DefaultLayout,
+		Ruler:  ruler,
+	})
+
+	// Render to SVG
+	out, err := d2svg.Render(diagram, &d2svg.RenderOpts{
+		Pad: d2svg.DEFAULT_PADDING,
+	})
+	if err != nil {
+		return nil, errors.New("Invalid D2 data.")
+	}
+	return out, nil
+}
+
+func main() {
+	router := vestigo.NewRouter()
+
+	router.Get("/svg/:encodedD2", GetD2SVGHandler)
+
+	log.Fatal(http.ListenAndServe(":8090", router))
+}
